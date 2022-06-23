d49a2a224bcea8b6571db8646cb9d4bc4989d83e
Arya Salsabila <aryasalsabila789@gmail.com>
2022-06-23 10:59:27 +0700

    Update deploy.sh

--- extract/deploy.sh
+++ extract/deploy.sh
@@ -2,6 +2,6 @@ gcloud functions deploy ocr-upload \
   --runtime python39 \
   --trigger-resource YOUR_TRIGGER_BUCKET_NAME \
   --trigger-event google.storage.object.finalize \
-  --entry-point parse_multipart \
+  --entry-point process_image \
   --allow-unauthenticated \
   --set-env-vars GCP_PROJECT=YOUR_GCP_PROJECT_ID,EXTRACT_TOPIC=YOUR_EXTRACT_TOPIC_NAME
