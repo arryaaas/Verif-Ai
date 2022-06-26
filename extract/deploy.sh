8efe42b5ebfdc783a7890a1b41badb22c5c7f9e3
arryaaas <aryasalsabila789@gmail.com>
2022-06-26 21:40:04 +0700

    delete deploy.sh

--- extract/deploy.sh
+++
@@ -1,7 +0,0 @@
-gcloud functions deploy ocr-extract \
-  --runtime python39 \
-  --trigger-resource YOUR_TRIGGER_BUCKET_NAME \
-  --trigger-event google.storage.object.finalize \
-  --entry-point process_image \
-  --allow-unauthenticated \
-  --set-env-vars GCP_PROJECT=YOUR_GCP_PROJECT_ID,EXTRACT_TOPIC=YOUR_EXTRACT_TOPIC_NAME
