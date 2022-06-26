9270f517d1d6192b08848eb4057251ac2cd3d182
arryaaas <aryasalsabila789@gmail.com>
2022-06-26 21:39:51 +0700

    update main.py

--- extract/main.py
+++ extract/main.py
@@ -1,5 +1,6 @@
 import os
 import json
+from itertools import groupby
 from google.cloud import vision
 from google.cloud import pubsub_v1
 
@@ -35,8 +36,6 @@ def publish_message(message):
 def detect_text(bucket, filename):
     """ Helper function that detects text in images using Cloud Vision API """
 
-    print(f"Looking for text in image {filename}")
-
     gcs_image_uri = f"gs://{bucket}/{filename}"
 
     image = vision.Image(
@@ -47,17 +46,58 @@ def detect_text(bucket, filename):
 
     vision_client = vision.ImageAnnotatorClient()
     text_detection_response = vision_client.text_detection(image=image)
-    annotations = text_detection_response.text_annotations
+    text_annotations = text_detection_response.text_annotations
+    text_annotations.pop(0)
 
-    if len(annotations) > 0:
-        text = annotations[0].description
-    else:
-        text = ""
+    text_annotations = list(
+        map(
+            lambda z: {
+                "description": z.description,
+                "vertex_x": z.bounding_poly.vertices[3].x,
+                "vertex_y": int((z.bounding_poly.vertices[3].y) / 10)
+            },
+            text_annotations
+        )
+    )
+
+    text_annotations = sorted(
+        text_annotations, 
+        key=lambda z: (z["vertex_y"], z["vertex_x"])
+    )
+
+    reference_vertex_y = 0
+    for i in range(len(text_annotations)):
+        vertices_y = [
+            reference_vertex_y - 1,
+            reference_vertex_y,
+            reference_vertex_y + 1
+        ]
+
+        if text_annotations[i]["vertex_y"] not in vertices_y:
+            reference_vertex_y = text_annotations[i]["vertex_y"]
+
+        text_annotations[i]["vertex_y"] = reference_vertex_y
+
+    text_annotations_group = []
+    for key, value in groupby(text_annotations, key=lambda z: z["vertex_y"]):
+        value = sorted(value, key=lambda z: z["vertex_x"])
+        text_annotations_group.append((list(value)))
+
+    for i in range(len(text_annotations_group)):
+        if len(text_annotations_group[i]) == 1:
+            text_annotations_group[i-1].append(text_annotations_group[i][0])
+
+    text_annotations_group = list(
+        filter(
+            lambda z: len(z) != 1, text_annotations_group
+        )
+    )
 
     # Print message to logs
-    print(f"Extracted text {text} from image {filename}")
+    print(f"Extracted text from image {filename}")
+    print(text_annotations_group)
 
-    return text
+    return text_annotations_group
 
 # Entry point function
 def process_image(event, context):
@@ -71,10 +111,10 @@ def process_image(event, context):
     name = validate_message(event, "name")
 
     # Text detection using Cloud Vision API
-    text = detect_text(bucket, name)
+    text_annotations_group = detect_text(bucket, name)
 
     message = {
-        "text": text
+        "text_annotations_group": text_annotations_group
     }
 
     # Publish message to Cloud Pub/Sub topics
